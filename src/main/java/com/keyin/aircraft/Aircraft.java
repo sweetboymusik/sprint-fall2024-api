a1073b4c1853d424add7efccc305f23cd864e2ed
sweetboymusik <ellbutt@gmail.com>
2024-10-30 14:44:40 -0230

    added Aircraft base class

--- src/main/java/com/keyin/aircraft/Aircraft.java
+++ src/main/java/com/keyin/aircraft/Aircraft.java
@@ -9,6 +9,52 @@ public class Aircraft {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    private String type;
+    private int passengerCapacity;
+
     @ManyToOne
     private Airline airline;
+
+    // constructors
+    public Aircraft() {
+    }
+
+    public Aircraft(String type, int passengerCapacity, Airline airline) {
+        this.type = type;
+        this.passengerCapacity = passengerCapacity;
+        this.airline = airline;
+    }
+
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public Airline getAirline() {
+        return airline;
+    }
+
+    public void setAirline(Airline airline) {
+        this.airline = airline;
+    }
 }
